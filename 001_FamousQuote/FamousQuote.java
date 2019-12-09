db7ce25c8ee960f921a03afc095dc4dafee25f11
Izzi Lauer <37642415+izziL@users.noreply.github.com>
2019-12-09 09:54:21 -0600

    Prints out a quote.

--- 001_FamousQuote/FamousQuote.java
+++ 001_FamousQuote/FamousQuote.java
@@ -1,6 +1,6 @@
 public class FamousQuote {
     public static void main(String[] args) {
-        // Print out the quote and the author on separate lines. \" makes a quotation mark inside the println, \n makes it move to the next line, and \t makes a tab.
+        // Print out the quote and the author on separate lines. \" makes a quotation mark inside the println, \n makes a new line, and \t makes a tab.
         System.out.println("\"I have no special talent. I am only passionately curious.\"\n\t\t- Albert Einstein");
     }
 }
