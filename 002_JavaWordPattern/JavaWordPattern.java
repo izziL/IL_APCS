47e12f05514e45d628ea6d39fad9330ee2f2de3f
Izzi Lauer <37642415+izziL@users.noreply.github.com>
2019-12-10 09:23:17 -0600

    Prints out 4 line tall "Java" to the console.

--- 002_JavaWordPattern/JavaWordPattern.java
+++ 002_JavaWordPattern/JavaWordPattern.java
@@ -1,6 +1,6 @@
 public class JavaWordPattern {
     public static void main(String[] args) {
-        // Print out the characters, each on a separate line.
+        // Print out the characters on separate lines.
         System.out.println("    J     A   V     V   A");
         System.out.println("    J    A A   V   V   A A");
         System.out.println("J   J   AAAAA   V V   AAAAA");
