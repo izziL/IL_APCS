bbebdc1558e9590f5374ddb2d5035bed40b0fb0e
Izzi Lauer <37642415+izziL@users.noreply.github.com>
2019-12-11 09:38:53 -0600

    Calculates the value of an expression

--- 007_ComputeAnExpression/ComputeAnExpression.java
+++ 007_ComputeAnExpression/ComputeAnExpression.java
@@ -1,6 +1,6 @@
 public class ComputeAnExpression {
     public static void main(String[] args) {
-        // Use a double to store the number and then print it out. Make sure to use parentheses to compute stuff in the right order.
+        // Use a double to store the number, then print it out. Make sure to use parentheses to compute stuff in the right order.
         double answer = ((9.5 * 4.5)-(2.5*3))/(45.5-3.5);
         System.out.println("The answer to the expression is " + answer + ".");
     }
