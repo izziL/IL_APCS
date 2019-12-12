227011cb08679c7dafe1991f9268a160596e4950
Izzi Lauer <37642415+izziL@users.noreply.github.com>
2019-12-12 09:37:09 -0600

    Overflow and underflow

--- 017_MaximumIntegers/MaximumIntegers.java
+++ 017_MaximumIntegers/MaximumIntegers.java
@@ -6,7 +6,7 @@ public class MaximumIntegers {
         int minInt = Integer.MIN_VALUE;
         // Print out the value of the max and min integers.
         System.out.println("The maximum value of an integer is " + maxInt + " and the minimum value of an integer is " + minInt + ".");
-        // Overflow the maximum to make 1 million.
+        // Overflow the maximum to get 1 million.
         maxInt += 2_147_000_000;
         maxInt += 1_483_649;
         System.out.println("The variable maxInt is equal to " + maxInt);
