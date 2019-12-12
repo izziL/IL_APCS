80ddb876e94553b20edaa9b66b0f0fb01815e851
Izzi Lauer <37642415+izziL@users.noreply.github.com>
2019-12-12 09:41:25 -0600

    Gives you heads or tails

--- 021_FlipACoin/FlipACoin.java
+++ 021_FlipACoin/FlipACoin.java
@@ -2,7 +2,7 @@ public class FlipACoin {
     public static void main(String[] args) {
         // Generate a random number: either 0 or 1.
         int number = (int) (Math.random() * 2);
-        // Make this into heads or tails.
+        // Change this into heads or tails.
         if (number == 0) {
             System.out.println("The result of the coin fip is heads.");
         } else {
