93d178b5c9f0829883477280ccd843b19d2401dd
Isabella Lauer <il1820187@028Labs-iMac.local>
2020-01-08 10:08:50 -0600

    Runs custom hello world method

--- 034_HelloWorldButANewMethod/HelloWorldButANewMethod.java
+++ 034_HelloWorldButANewMethod/HelloWorldButANewMethod.java
@@ -1,7 +1,12 @@
-public void HelloWorld() {
-  System.out.println("Hello World!");
+public class HelloWorldButANewMethod {
+  // Make a hello world class.
+  public static void HelloWorld() {
+    System.out.println("Hello World!");
+  }
+
+  public static void main(String[] args) {
+    // Run the HelloWorld() method.
+    HelloWorld();
+  }
 }
 
-public static void main(String[] args) {
-  HelloWorld();
-}
