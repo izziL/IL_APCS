05ef067f00cc0242b944f3f88a279ed82f842553
Isabella Lauer <il1820187@028Labs-iMac.local>
2020-01-10 10:20:35 -0600

    Still owrking on stuff

---
+++ 036_CustomCalculator/CustomCalculator.java
@@ -0,0 +1,16 @@
+public class CustomCalculator {
+    // Custom methods
+    public static double add(double a, double b) {
+        return sum;
+    }
+    public static double subtract(double a, double b) {
+        return (a - b);
+    }
+    public static double multiple(double a, double b) {
+        reurn
+    }
+    // Execute custom methods
+    public static void main(String[] args) {
+
+    }
+}
