5787a3cad55849e632b62c305a6c662581e28d2b
Isabella Lauer <il1820187@028Labs-iMac.local>
2020-01-14 10:00:13 -0600

    add + multiply methods work

--- 036_CustomCalculator/CustomCalculator.java
+++ 036_CustomCalculator/CustomCalculator.java
@@ -8,7 +8,7 @@ public class CustomCalculator {
     public static double subtract(double a, double b) {
         return (a - b);
     }
-    public static double multiple(double a, double b) {
+    public static double multiply(double a, double b) {
         return (a * b);
     }
     public static double divide(double a, double b) {
@@ -30,24 +30,54 @@ public class CustomCalculator {
     public static void main(String[] args) {
         // Variables
         int choice;
-        String choiceName;
+        String choiceName = "";
+        double numOne;
+        double numTwo = 0;
         // Ask what the user wants to do.
         Scanner scanner = new Scanner(System.in);
         System.out.println("What would you like the calculator to do? Please enter the corresponding number: \n1) Add two numbers \n2) Subtract two numbers \n3) Multiply two numbers \n4) Divide two numbers \n5) Raise x to the yth power \n6) Take the log (base 10) of a number \n7) Take the natural log of a number \n8) Raise e to the xth power");
         choice = scanner.nextInt();
         scanner.nextLine();
         // Run the function they want to run.
-        if (choice == 1 || choice == 2 || choice == 3 || choice == 4) {
+        if (choice == 1 || choice == 3) {
             if (choice == 1) {
                 choiceName = "add";
-            } else if (choice == 1) {
-                choiceName = "subtract";
-            } else if (choice == 1) {
-                choiceName = "multiply";
             } else {
-                choiceName = "divide";
+                choiceName = "multiply";
             }
             System.out.print("What is the first number you would like to " + choiceName + "? ");
-        }
+            numOne = scanner.nextDouble();
+            scanner.nextLine();
+            System.out.print("What is the second number you would like to " + choiceName + "? ");
+            numTwo = scanner.nextDouble();
+            System.out.print("The answer is ");
+            if(choiceName.equals("add")) {
+                System.out.println(add(numOne, numTwo) + ".");
+            } else {
+                System.out.println(multiply(numOne, numTwo) + ".");
+            }
+        } else if (choice == 2) {
+            System.out.print("What number would you like to subtract from? (minuend) ");
+            numOne = scanner.nextDouble();
+            scanner.nextLine();
+            System.out.print("What number would you like to subtract? (subtrahend) ");
+            numTwo = scanner.nextDouble();
+            System.out.println("The answer is " + subtract(numOne, numTwo));
+        } else if (choice == 4) {
+            System.out.print("What number would you like to divide? (dividend) ");
+            numOne = scanner.nextDouble();
+            scanner.nextLine();
+            System.out.print("What would you like the divisor to be? ");
+            numTwo = scanner.nextDouble();
+            System.out.println("The answer is " + divide(numOne, numTwo) + ".");
+        } else if (choice == 5) {
+            System.out.print("What would you like the base to be? ");
+            numOne = scanner.nextDouble();
+            scanner.nextLine();
+            System.out.print("What would you like the exponent to be? ");
+            numTwo = scanner.nextDouble();
+            System.out.println("The answer is " + exponent(numOne, numTwo) + ".");
+        } 
+        scanner.close();
     }
 }
