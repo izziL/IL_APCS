a32b45e335b03eab9289ac9021af2ea1d725ed8f
Isabella Lauer <il1820187@028Labs-iMac.local>
2020-01-30 10:12:07 -0600

    Not even close to done

--- 038_BlackjackTwo/CardDispenser.java
+++ 038_BlackjackTwo/CardDispenser.java
@@ -1,24 +1,43 @@
 public class CardDispenser {
     // Variables
-    int number;
-    int suitNum;
-    String suit;
-    // This method returns a random card. There are no jacks, queens or kings, but 10s are 4 times as likely to show up as any other number.
+    private String card;
+    private int cardValue;
+    private int suitNum;
+    private String suit;
+    // This method returns a random card.
     public String newCard() {
-        number = (int) (Math.random() * 13 + 1);
-        if (number > 10) {
-            number = 10;
+        cardValue = (int) (Math.random() * 13 + 1);
+        if (cardValue < 11) {
+            card = Integer.toString(cardValue);
+        } else if (cardValue == 11) {
+            cardValue = 10;
+            card = "Jack";
+        } else if (cardValue == 12) {
+            cardValue = 10;
+            card = "Queen";
+        } else if (cardValue == 13) {
+            cardValue = 10;
+            card = "King";
+        }
+        if (cardValue == 1) {
+            cardValue = 11;
+            card = "Ace";
         }
         suitNum = (int) (Math.random() * 4 + 1);
         if (suitNum == 1) {
-            suit = "hearts";
+            suit = "Hearts";
         } else if (suitNum == 2) {
-            suit = "diamonds";
+            suit = "Diamonds";
         } else if (suitNum == 3) {
-            suit = "spades";
+            suit = "Spades";
         } else {
-            suit = "clubs";
+            suit = "Clubs";
         }
-        return number + " of " + suit;
+        return card + " of " + suit;
+    }
+    public int getValue(String card) {
+        String stringCardValue = card.substring(0, 1);
+        int finalCardValue = Integer.parseInt(stringCardValue);
+        return finalCardValue;
     }
 }
--- 038_BlackjackTwo/Table.java
+++ 038_BlackjackTwo/Table.java
@@ -1,9 +1,18 @@
 public class Table {
+    public static void playBlackjack() {
+        // Variables
+        String playerCardOne;
+        String playerCardTwo;
+        // Blackjack Rules
+        System.out.println("HOW TO PLAY BLACKJACK\nThe goal of Blackjack is to get as close as possible to 21 without going over. You will get two cards at first. Then you decide if you want to get another card or not. At the beginning, aces will count as 11. If you draw another card and go over 21, but you have an 11, the 11 will be changed to a one.");
+        // Give two cards to the player and tell them their point value
+        CardDispenser dispenser = new CardDispenser();
+        playerCardOne = dispenser.newCard();
+        playerCardTwo = dispenser.newCard();
+        System.out.println("hi");
+        //System.out.println("You have been dealt two cards: " + dispenser.getValue(playerCardOne) + " and " + dispenser.getValue(playerCardTwo) /*+ ". The value of your cards is " + (dispenser.getValue(playerCardOne) + dispenser.getValue(playerCardTwo)) + "."*/);
+    }
     public static void main(String[] args) {
-        CardDispenser one = new CardDispenser();
-        String a = one.newCard();
-        System.out.println(a);
-        String b = one.newCard();
-        System.out.println(b);
+        playBlackjack();
     }
 }
