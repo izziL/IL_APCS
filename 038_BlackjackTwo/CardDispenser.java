4cdcec42efb4e3590c08d9d8a8c3a2eb81050441
Isabella Lauer <il1820187@028Labs-iMac.local>
2020-01-31 10:13:51 -0600

    Done with CardDispenser, still working on Table
    I know there is probably a better way to write CardDispenser, because dealCards() in Table.java is really repetitive, but at this point, I just want to get done with the assignment, and my program works fine.

--- 038_BlackjackTwo/CardDispenser.java
+++ 038_BlackjackTwo/CardDispenser.java
@@ -4,23 +4,32 @@ public class CardDispenser {
     private int cardValue;
     private int suitNum;
     private String suit;
+    // This method returns a random number.
+    public int newNum() {
+        return (int) (Math.random() * 13 + 1);
+    }
+    // This method returns the point value of a card.
+    public int pointValue(int cardValue) {
+        if (cardValue > 10) {
+            return 10;
+        } else if (cardValue == 1) {
+            return 11;
+        } else {
+            return cardValue;
+        }
+    }
     // This method returns a random card.
-    public String newCard() {
-        cardValue = (int) (Math.random() * 13 + 1);
-        if (cardValue < 11) {
-            card = Integer.toString(cardValue);
-        } else if (cardValue == 11) {
-            cardValue = 10;
+    public String newCard(int valueOfCard) {
+        if (valueOfCard < 11) {
+            card = Integer.toString(valueOfCard);
+        } else if (valueOfCard == 11) {
             card = "Jack";
-        } else if (cardValue == 12) {
-            cardValue = 10;
+        } else if (valueOfCard == 12) {
             card = "Queen";
-        } else if (cardValue == 13) {
-            cardValue = 10;
+        } else if (valueOfCard == 13) {
             card = "King";
         }
-        if (cardValue == 1) {
-            cardValue = 11;
+        if (valueOfCard == 1) {
             card = "Ace";
         }
         suitNum = (int) (Math.random() * 4 + 1);
@@ -34,10 +43,5 @@ public class CardDispenser {
             suit = "Clubs";
         }
         return card + " of " + suit;
-    }
-    public int getValue(String card) {
-        String stringCardValue = card.substring(0, 1);
-        int finalCardValue = Integer.parseInt(stringCardValue);
-        return finalCardValue;
     }
 }
