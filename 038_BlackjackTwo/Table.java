a71dece962010ea7b5722e5a3f4ce634d95896d0
Isabella Lauer <izzi.lauer@gmail.com>
2020-02-06 10:20:20 -0600

    more stuff in Table.java

--- 038_BlackjackTwo/Table.java
+++ 038_BlackjackTwo/Table.java
@@ -13,15 +13,30 @@ public class Table {
         dealerCardOne.fixValue();
         System.out.println("The dealer's card is: " + dealerCardOne.getCard() + ". It has a value of " + dealerCardOne.getValue() + ".");
     }
-
+    // This method makes a new card
+    public static int makeNewCard() {
+        CardDispenser newCard = new CardDispenser();
+        newCard.fixValue();
+        System.out.println("You received a new card: " + newCard.getCard() + ". The value of your new card is " + newCard.getValue() + ".");
+        return newCard.getValue();
+    }
+    // This method is what happens when the user wants to hit to get another card.
     public static void hitOrStay() {
+        String hitOrStay;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Would you like to hit or stay? If you would like to hit, please type in \"hit.\"");
-        
+        hitOrStay = scanner.nextLine().strip().toLowerCase();
+        if (hitOrStay.equals("hit")) {
+            makeNewCard();
+        }
         scanner.close();
+    }
+    // This plays the game using the other custom methods.
+    public static void playGame() {
+        dealOriginalCards();
     }
 
     public static void main(String[] args) {
-        dealOriginalCards();
+        playGame();
     }
 }
