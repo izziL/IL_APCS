6fb18988070d37d13e1c3cb1bf4e991abda529ee
Isabella Lauer <izzi.lauer@gmail.com>
2020-02-14 09:51:34 -0600

    Refactoring 023_BlackjackOne
    I made a class that generates a random card. The main program is composed of multiple small methods.

--- 038_BlackjackTwo/Table.java
+++ 038_BlackjackTwo/Table.java
@@ -1,42 +1,72 @@
 import java.util.Scanner;
 
 public class Table {
-    public static void dealOriginalCards() {
+    Scanner scanner = new Scanner(System.in);
+    public int dealCardsPlayer() {
         // Give the player two cards
         CardDispenser playerCardOne = new CardDispenser();
         playerCardOne.fixValue();
         CardDispenser playerCardTwo = new CardDispenser();
         playerCardTwo.fixValue();
         System.out.println("You received two cards: " + playerCardOne.getCard() + " and " + playerCardTwo.getCard() + ". The value of your cards is " + (playerCardOne.getValue() + playerCardTwo.getValue()) + ".");
+        // Return the point value of the player
+        return (playerCardOne.getValue() + playerCardTwo.getValue());
+    }
+    public int dealCardDealer() {
         // Give the dealer one card
         CardDispenser dealerCardOne = new CardDispenser();
         dealerCardOne.fixValue();
         System.out.println("The dealer's card is: " + dealerCardOne.getCard() + ". It has a value of " + dealerCardOne.getValue() + ".");
+        return dealerCardOne.getValue();
     }
     // This method makes a new card
-    public static int makeNewCard() {
+    public int makeNewCard() {
         CardDispenser newCard = new CardDispenser();
         newCard.fixValue();
-        System.out.println("You received a new card: " + newCard.getCard() + ". The value of your new card is " + newCard.getValue() + ".");
+        System.out.println("The new card is: " + newCard.getCard() + ". The value of the new card is " + newCard.getValue() + ".");
         return newCard.getValue();
     }
-    // This method is what happens when the user wants to hit to get another card.
-    public static void hitOrStay() {
-        String hitOrStay;
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Would you like to hit or stay? If you would like to hit, please type in \"hit.\"");
-        hitOrStay = scanner.nextLine().strip().toLowerCase();
-        if (hitOrStay.equals("hit")) {
-            makeNewCard();
+    // This plays the game using the other custom methods. I know this method is really long but I'm tired of working on this assignment.
+    public void playGame() {
+        System.out.println("HOW TO PLAY BLACKJACK\nThe goal of Blackjack is to get as close as possible to 21 without going over. You will get two cards at first. Then you decide if you want to get another card or not. Aces will count as 1.\n");
+        int totalPlayerPoints = dealCardsPlayer();
+        int totalDealerPoints = dealCardDealer();
+        boolean keepPlaying = true;
+        String hitStay;
+        while (totalPlayerPoints < 21 && keepPlaying) {
+            System.out.print("Would you like to hit or stay? If you would like to hit, please type in \"hit.\" ");
+            hitStay = scanner.nextLine().strip().toLowerCase();
+            if (!hitStay.equals("hit")) {
+                keepPlaying = false;
+            } else {
+                totalPlayerPoints += makeNewCard();
+                System.out.println("Your new total is " + totalPlayerPoints + ".");
+            }
         }
-        scanner.close();
+        if (totalPlayerPoints == 21) {
+            System.out.println("Good job, you won!");
+        } else if (totalPlayerPoints > 21) {
+            System.out.println("Sorry, you lost.");
+        } else {
+            System.out.println("\nDealer's turn");
+            while (totalDealerPoints < 17) {
+                totalDealerPoints += makeNewCard();
+                System.out.println("The dealer's new total is " + totalDealerPoints + ".");
+            }
+            if (totalDealerPoints > 21) {
+                System.out.println("Good job, you won!");
+            } else if (totalPlayerPoints > totalDealerPoints) {
+                System.out.println("Good job, you won!");
+            } else if (totalDealerPoints == totalPlayerPoints) {
+                System.out.println("You tied.");
+            } else {
+                System.out.println("Sorry, you lost.");
+        }
+        }
+        
     }
-    // This plays the game using the other custom methods.
-    public static void playGame() {
-        dealOriginalCards();
-    }
-
     public static void main(String[] args) {
-        playGame();
+        Table game = new Table();
+        game.playGame();
     }
 }
