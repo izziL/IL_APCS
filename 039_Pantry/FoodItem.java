b38782e6e7e4e8ca77d9048a03d87bc109e2cd9b
Isabella Lauer <izzi.lauer@gmail.com>
2020-02-20 10:19:36 -0600

    Eating food from a pantry
    FoodItem.java contains a constructor and accessor & mutator methods. PantryShelf.java runs custom methods to decrease the number of a food created from FoodItem

--- 039_Pantry/FoodItem.java
+++ 039_Pantry/FoodItem.java
@@ -1,12 +1,12 @@
 public class FoodItem {
     // Variables for the types and amounts of food
-    private double amount;
+    private int amount;
     private String amountUnit;
     private String color;
-    private double weight;
+    private String weight;
     private double cost;
     // Creating the food
-    public newFood(double number, String amountUnit, String foodColor, String foodWeight, double foodCost) {
+    public FoodItem(int number, String numberUnit, String foodColor, String foodWeight, double foodCost) {
         amount = number;
         amountUnit = numberUnit;
         color = foodColor;
@@ -14,7 +14,7 @@ public class FoodItem {
         cost = foodCost;
     }
     // Accessor methods
-    public double getAmount() {
+    public int getAmount() {
         return amount;
     }
     public String getAmountUnit() {
@@ -23,14 +23,14 @@ public class FoodItem {
     public String getColor() {
         return color;
     }
-    public double getWeight() {
+    public String getWeight() {
         return weight;
     }
-    public double cost() {
+    public double getCost() {
         return cost;
     }
-    // Mutator method. The mutator method is only needed to change the amount of the item.
-    public void setAmount(double newAmount) {
+    // Mutator method. The only mutator method needed is to change the amount of the item.
+    public void setAmount(int newAmount) {
         amount = newAmount;
     }
 }
--- 039_Pantry/PantryShelf.java
+++ 039_Pantry/PantryShelf.java
@@ -1,5 +1,28 @@
 public class PantryShelf {
+    public void getInformation(FoodItem food) {
+        System.out.println("This food item is: " + food.getAmountUnit() + ". There are " + food.getAmount() + " " + food.getAmountUnit() + ". The color of the food is " + food.getColor() + ". The total weight of the food is " + food.getWeight() + ". The cost of the food is $" + food.getCost() + ".");
+    }
+    public void eatFood(FoodItem food) {
+        int amountEaten = (int) (Math.random() * food.getAmount() + 1);
+        System.out.println("I want to eat " + amountEaten + " " + food.getAmountUnit() + ".");
+        while (amountEaten > 0) {
+            food.setAmount(food.getAmount() - 1);
+            System.out.println("I ate a " + food.getAmountUnit() + ". There are " + food.getAmount() + " " + food.getAmountUnit() + " left. I want to eat " + (amountEaten - 1) + " more.");
+            amountEaten--;
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("hi");
+        // Create food items
+        FoodItem apple = new FoodItem(10, "apple", "red", "5 lbs", 5.00);
+        FoodItem blueberry = new FoodItem(30, "blueberry", "blue", "3 oz", 3.50);
+        FoodItem carrot = new FoodItem(15, "baby carrot", "orange", "3 oz", 1.50);
+        // Eat the food
+        PantryShelf pantry = new PantryShelf();
+        pantry.getInformation(apple);
+        pantry.eatFood(apple);
+        pantry.getInformation(blueberry);
+        pantry.eatFood(blueberry);  
+        pantry.getInformation(carrot);
+        pantry.eatFood(carrot);
     }
 }
