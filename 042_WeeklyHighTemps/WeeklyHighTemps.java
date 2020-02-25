9bed0b5a7d505a7b946b967b0ba1f47eca7c7894
Isabella Lauer <izzi.lauer@gmail.com>
2020-02-25 09:56:17 -0600

    Accessing values from an array

---
+++ 042_WeeklyHighTemps/WeeklyHighTemps.java
@@ -0,0 +1,13 @@
+public class WeeklyHighTemps {
+    public static void main(String[] args) {
+        // Array with temperatures
+        int[] temperature = {25, 20, 18, 21, 29};
+        // Print out the values
+        System.out.println("Weather for the next five days:");
+        System.out.println("Day One: " + temperature[0] + " degrees.");
+        System.out.println("Day Two: " + temperature[1] + " degrees.");
+        System.out.println("Day Three: " + temperature[2] + " degrees.");
+        System.out.println("Day Four: " + temperature[3] + " degrees.");
+        System.out.println("Day Five: " + temperature[4] + " degrees.");
+    }
+}
