36ee42014103a14329f71aaace3d2d00eacf8e84
Isabella Lauer <izzi.lauer@gmail.com>
2020-03-04 11:03:34 -0600

    Adds the positive and negative numbers in an array separately.

---
+++ 045_OneThousandPositiveOrNegativeInts/SumPositiveOrNegative.java
@@ -0,0 +1,18 @@
+public class SumPositiveOrNegative {
+    public static void main(String[] args) {
+        int sumOfPositives = 0;
+        int sumOfNegatives = 0;
+        // Get array
+        OneThousandPositiveOrNegativeInts numberArray = new OneThousandPositiveOrNegativeInts();
+        int[] array = numberArray.getOneThousandPositiveOrNegativeInts();
+        // Calculate
+        for (int number : array) {
+            if (number > 0) {
+                sumOfPositives += number;
+            } else {
+                sumOfNegatives += number;
+            }
+        }
+        System.out.println("The sum of the positives is " + sumOfPositives + ", and the sum of the negatives is " + sumOfNegatives + ".");
+    }
+}
